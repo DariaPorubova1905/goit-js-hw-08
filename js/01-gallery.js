6f72621f70adea27048d8bb2cf3b70165721a0f1
DariaPorubova1905 <102602876+DariaPorubova1905@users.noreply.github.com>
2023-08-10 21:46:30 +0300

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,33 +1,18 @@
-// Add imports above this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-// Описаний в документації
-import SimpleLightbox from 'simplelightbox';
+const ulRef = document.querySelector('.gallery');
+const galleryMarkup = galleryItems.map(({preview, original, description})=>
+`<li class="gallery__item">
+<a class="gallery__link" href="${original}">
+   <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</li>`).join('');
+ulRef.innerHTML = galleryMarkup;
 
-// Додатковий імпорт стилів
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
-const ulGallery = document.querySelector('.gallery');
-const markup = createGalleryMarkup(galleryItems);
-
-function createGalleryMarkup(items) {
-  return items
-    .map(
-      item => `<li class="gallery__item">
-   <a class="gallery__link" href="${item.original}">
-      <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
-   </a>
-</li>`
-    )
-    .join('');
-}
-
-ulGallery.insertAdjacentHTML('beforeend', markup);
-
-let galleryLightbox = new SimpleLightbox('.gallery a', {
-  captions: true,
-  captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionDelay: 200,
-});
+const opts = {
+   captionsData: 'alt',
+   captionDelay: 250,
+};
+const lightbox = new SimpleLightbox('.gallery a',opts);
